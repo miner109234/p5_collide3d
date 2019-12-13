5183e69c9e64220f7f750aa3d1e6c255fbfcfe69
miner109234 <andy.yaccarino@gmail.com>
2019-12-13 08:38:15 -0600

    Update
    Added collidePlanePlane

--- p5_collide_3d.js
+++ p5_collide_3d.js
@@ -1,4 +1,4 @@
-p5.prototype.collideSphereSphere(x1,y1,z1,r1, x2,y2,z2,r2) {
+p5.prototype.collideSphereSphere = function(x1,y1,z1,r1, x2,y2,z2,r2) {
     // Calculate the distance between both spheres
     let dx = x2 - x1;
     let dy = y2 - y1;
@@ -8,19 +8,19 @@ p5.prototype.collideSphereSphere(x1,y1,z1,r1, x2,y2,z2,r2) {
     return dist <= r1 + r2; // returns true iff they are touching or overlapping
 };
 
-p5.prototype.collideSpherePlane(spos,sr, points, mode) {
+p5.prototype.collideSpherePlane = function(spos,sr, points, mode) {
     // There are multiple ways of checking if a sphere is intersecting a plane... this is determined by the mode
     mode = mode || 0;
     if (mode === 0) { // points are all points on the plane
         if (points.length < 3) throw new SyntaxError("\"Array 'points' must contain three points on the plane in this mode!\" - collideSpherePlane");
         // Calculate the plane's normal
-        let a = p2.sub(p1);
-        let b = p3.sub(p1);
+        let a = points[1].sub(points[0]);
+        let b = points[2].sub(points[0]);
         let normal = a.cross(b);
         normal = normal.normalize(); // because normals are always a unit vector
         
         // calculate the plane's D constant
-        let D = normal.dot(p1);
+        let D = normal.dot(points[0]);
         
         // calculate the distance
         let dist = Math.abs(normal.dot(spos) - D);
@@ -39,5 +39,57 @@ p5.prototype.collideSpherePlane(spos,sr, points, mode) {
         let dist = Math.abs(points[0].dot(spos) - points[1]);
         
         return dist <= sr; // collsion
+    }
+};
+
+p5.prototype.collidePlanePlane = function(plane1Points, plane2Points, mode) {
+    // There are multiple ways of checking if a plane is intersecting a plane... this is determined by the mode
+    mode = mode || 0;
+    
+    // planes are colliding iff the normals are not inline (extention of the fact parrallel lines don't intersect)
+    if (mode === 0) { // two arrays of 3 points
+        if (plane1Points.length < 3) throw new SyntaxError("\"Array 'plane1Points' must contain three points on the plane in this mode!\" - collidePlanePlane");
+        if (plane2Points.length < 3) throw new SyntaxError("\"Array 'plane2Points' must contain three points on the plane in this mode!\" - collidePlanePlane");
+        // Calculate the plane1's normal
+        let a = plane1Points[1].sub(plane1Points[0]);
+        let b = plane1Points[2].sub(plane1Points[0]);
+        let normal1 = a.cross(b);
+        normal1 = normal1.normalize(); // because normals are always a unit vector
+        // Calculate the plane2's normal
+        a = plane2Points[1].sub(plane2Points[0]);
+        b = plane2Points[2].sub(plane2Points[0]);
+        let normal2 = a.cross(b);
+        normal2 = normal2.normalize(); // because normals are always a unit vector
+        
+        // they are parrallel iff n1 dot n2 == 0
+        return normal1.dot(normal2) !== 0;
+    } else (mode == 1) {
+        if (plane1Points instanceof Array) throw new SyntaxError("\"'plane1Points' must be the plane's normal in this mode, not an Array!\" - collidePlanePlane");
+        if (plane2Points.length < 3) throw new SyntaxError("\"Array 'plane2Points' must contain three points on the plane in this mode!\" - collidePlanePlane");
+        // Calculate the plane2's normal
+        let a = plane2Points[1].sub(plane2Points[0]);
+        let b = plane2Points[2].sub(plane2Points[0]);
+        let normal2 = a.cross(b);
+        normal2 = normal2.normalize(); // because normals are always a unit vector
+        
+        // they are parrallel iff n1 dot n2 == 0
+        return plane1Points.dot(normal2) !== 0;
+    } else (mode == 2) {
+        if (plane1Points.length < 3) throw new SyntaxError("\"Array 'plane1Points' must contain three points on the plane in this mode!\" - collidePlanePlane");
+        if (plane2Points instanceof Array) throw new SyntaxError("\"'plane2Points' must be the plane's normal in this mode, not an Array!\" - collidePlanePlane");
+        // Calculate the plane2's normal
+        let a = plane1Points[1].sub(plane1Points[0]);
+        let b = plane1Points[2].sub(plane1Points[0]);
+        let normal1 = a.cross(b);
+        normal1 = normal1.normalize(); // because normals are always a unit vector
+        
+        // they are parrallel iff n1 dot n2 == 0
+        return normal1.dot(plane2Points) !== 0;
+    } else (mode == 3) {
+        if (plane1Points instanceof Array) throw new SyntaxError("\"'plane1Points' must be the plane's normal in this mode, not an Array!\" - collidePlanePlane");
+        if (plane2Points instanceof Array) throw new SyntaxError("\"'plane2Points' must be the plane's normal in this mode, not an Array!\" - collidePlanePlane");
+        
+        // they are parrallel iff n1 dot n2 == 0
+        return plane1Points.dot(plane2Points) !== 0;
     }
 };
